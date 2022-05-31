376f195c1735ce857578ffab92375303f0759d9e
teeteelaryo <titilayoodufowoke@gmail.com>
2022-05-31 21:07:51 +0100

    Files updated

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+def print_low_alpha_ex():
+    for n in range(97, 123):
+        if n != 101 and n != 113:
+            print('{}'.format(chr(n)), end='')
+
+
+if __name__ == '__main__':
+    print_low_alpha_ex()
