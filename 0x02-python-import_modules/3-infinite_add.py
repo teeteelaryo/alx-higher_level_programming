41646992a8a96514ab8bbef9c988a9f2802ddcc6
teeteelaryo <titilayoodufowoke@gmail.com>
2022-06-05 00:02:08 +0100

    Files Added

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    total = 0
-    for i in range(1, len(sys.argv)):
-        total += int(sys.argv[i])
-    print(total)
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
     
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-
-if __name__ == '__main__':
-    for name in dir(hidden_4)[:]:
-        if not name.startswith('__'):
+if __name__ == "__main__":
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
             print(name)
             
