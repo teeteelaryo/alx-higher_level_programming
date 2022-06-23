9dd266cfa37c0dce352eca8906a67aab0be81876
teeteelaryo <titilayoodufowoke@gmail.com>
2022-06-23 02:21:02 +0100

    Files Added

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,93 +1,59 @@
 #!/usr/bin/python3
-"""Square related feature module."""
+"""Create a Class Square with size, method of area and getters & setters"""
 
 
 class Square:
-    """Class that define a Square."""
+    """Class - Square"""
 
     def __init__(self, size=0):
-        """
-            Args:
-                size (int): size initializer
-        """
+        """Constructor of a Square with the size"""
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+    def __lt__(self, other):
+        """Compare operator <"""
+        return (self.area() < other.area())
 
-        if size < 0:
-            raise ValueError("size must be >= 0")
+    def __le__(self, other):
+        """Compare operator <="""
+        return (self.area() <= other.area())
 
-        self.__size = size
+    def __gt__(self, other):
+        """Compare operator >"""
+        return (self.area() > other.area())
+
+    def __ge__(self, other):
+        """Compare operator >="""
+        return (self.area() >= other.area())
+
+    def __eq__(self, other):
+        """Compare operator =="""
+        return (self.area() == other.area())
+
+    def __ne__(self, other):
+        """Compare operator !="""
+        return (self.area() != other.area())
 
     def area(self):
-        """Compute the area of the Square.
-
-            Returns:
-                The area. An (integer)
-        """
-
+        """Method to get the area of the Square"""
         return (self.__size ** 2)
 
     @property
     def size(self):
-        """__size property getter"""
-        return self.__size
+        """Getter of the private attribute size"""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """__size property setter.
-
-            Args:
-                value (int): new size value
-
-            Raises:
-                TypeError: if `value` is not an integer
-                ValueError: if `value` is < 0
-        """
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-
-        if value < 0:
-            raise ValueError("size must be >= 0")
-
-        self.__size = value
-
-    def __operr_msg(self, oper, instance):
-        msg = "'{:s}' not supported between instances of '{:s}' and '{:s}'"
-        return msg.format(oper, type(self).__name__, type(instance).__name__)
-
-    def __eq__(self, instance):
-        """square1 == square2"""
-        if instance is not None and type(instance) is not Square:
-            raise TypeError(self.__operr_msg('==', instance))
-        return instance is not None and self.area() == instance.area()
-
-    def __ne__(self, instance):
-        """square1 != square2"""
-        if instance is not None and type(instance) is not Square:
-            raise TypeError(self.__operr_msg('!=', instance))
-        return instance is not None and self.area() != instance.area()
-
-    def __gt__(self, instance):
-        """square1 > square2"""
-        if type(instance) is not Square:
-            raise TypeError(self.__operr_msg('>', instance))
-        return self.area() > instance.area()
-
-    def __ge__(self, instance):
-        """square1 >= square2"""
-        if type(instance) is not Square:
-            raise TypeError(self.__operr_msg('>=', instance))
-        return self.area() >= instance.area()
-
-    def __lt__(self, instance):
-        """square1 < square2"""
-        if type(instance) is not Square:
-            raise TypeError(self.__operr_msg('<', instance))
-        return self.area() <= instance.area()
-
-    def __le__(self, instance):
-        """square1 <= square2"""
-        if type(instance) is not Square:
-            raise TypeError(self.__operr_msg('<=', instance))
-        return self.area() <= instance.area()
+        """Setter for the size private attribute"""
+        if ((type(value) is int) or (type(value) is float)):
+            if (value < 0):
+                raise (ValueError("size must be >= 0"))
+            else:
+                self.__size = value
+        else:
+            raise (TypeError("size must be a number"))
